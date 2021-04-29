0bf995e94485fea50216079094f76e78c2cd4812
jjitch <junnosuke_08@icloud.com>
2021-04-30 04:58:26 +0900

    create dev. env.

--- Graphics/main.cpp
+++ Graphics/main.cpp
@@ -1,41 +1,54 @@
-#include <iostream>
-#include <GLFW/glfw3.h>
+//#pragma comment(linker, "/subsystem:\"windows\" /entry:\"mainCRTStartup\"")
 
+#include <iostream>
+#include <cstdlib>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    if (glfwInit() == GL_FALSE)
-    {
-        std::cerr << "Can't initilize GLFW" << std::endl;
-        return 1;
-    }
+	if (glfwInit() == GL_FALSE)
+	{
+		std::cerr << "Can't initilize GLFW" << std::endl;
+		return 1;
+	}
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	std::atexit(glfwTerminate);
 
-    GLFWwindow* const window = glfwCreateWindow(640, 480, "SAMPLE", NULL, NULL);
-    if (window == nullptr)
-    {
-        std::cerr << "Can't create GLFW window." << std::endl;
-        glfwTerminate();
-        return 1;
-    }
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    glfwMakeContextCurrent(window);
+	GLFWwindow* const window = glfwCreateWindow(640, 480, "SAMPLE", NULL, NULL);
+	if (window == nullptr)
+	{
+		std::cerr << "Can't create GLFW window." << std::endl;
+		glfwTerminate();
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
 
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Can't initialize GLEW." << std::endl;
+		return 1;
+	}
 
-    while (glfwWindowShouldClose(window) == GL_FALSE)
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
+	glfwSwapInterval(1);
 
+	glClearColor(0.f, 0.f, 0.f, 0.2f);
 
-        glfwSwapBuffers(window);
+	while (glfwWindowShouldClose(window) == GL_FALSE)
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
 
-        glfwWaitEvents();
-    }
+		//implement drawing process here
 
-    glfwTerminate();
+		glfwSwapBuffers(window);
+
+		glfwWaitEvents();
+	}
+
 }
