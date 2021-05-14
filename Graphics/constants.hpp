a38585d4e54e7be622a21fad600ef9b7edf88e08
jjitch <junnosuke_08@icloud.com>
2021-05-14 19:05:24 +0900

    add some constants

--- Graphics/constants.hpp
+++ Graphics/constants.hpp
@@ -9,8 +9,13 @@ namespace C
 	constexpr GLclampf bgGreen = 0.f;
 	constexpr GLclampf bgBlue = 0.2f;
 	constexpr GLclampf bgAlpha = 0.f;
-	constexpr GLuint vertexDim = 2;
+	constexpr GLuint vertexDim = 3;
 	constexpr GLuint attrLoc = 0;
+	constexpr GLuint fragLoc = 0;
 	constexpr int winWidth = 640;
 	constexpr int winHeight = 480;
+	constexpr char winTitle[] = "OASOBI";
+	constexpr GLdouble initialScale = 200.;
+	constexpr GLdouble wheelSensitivity = 30.;
+	constexpr GLdouble dirKeySensitivity = 25.;
 }
